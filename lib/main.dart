93b78010ba2a9ecc80209aede9c1c3142d67429c
RemoCharles <remo.gemperli@hotmail.ch>
2020-10-30 15:06:55 +0100

    created main class

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:whes_tablet_app/styles.dart';
 
+void main() {
+  return runApp(MaterialApp(
+    title: 'WHES TABLET APP',
+    theme: ThemeData(
+        primaryColor: Styles.PrimaryColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity),
+  ));
+}
