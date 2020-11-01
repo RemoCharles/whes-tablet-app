f7eeea11026701dc2ed52a3487412697b6a51931
RemoCharles <remo.gemperli@hotmail.ch>
2020-11-01 17:16:11 +0100

    removed flag icon in language

--- lib/classes/language.dart
+++ lib/classes/language.dart
@@ -1,17 +1,16 @@
 class Language {
   final int id;
-  final String flag;
   final String name;
   final String languageCode;
 
-  Language(this.id, this.flag, this.name, this.languageCode);
+  Language(this.id, this.name, this.languageCode);
 
   static List<Language> languageList() {
     return <Language>[
-      Language(1, "DE", "Deutsch", "de"),
-      Language(2, "FR", "Francais", "fr"),
-      Language(3, "IT", "Italiano", "it"),
-      Language(4, "EN", "English", "en"),
+      Language(1, "Deutsch", "de"),
+      Language(2, "Francais", "fr"),
+      Language(3, "Italiano", "it"),
+      Language(4, "English", "en"),
     ];
   }
 }
