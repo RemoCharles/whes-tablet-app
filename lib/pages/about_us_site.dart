40ffdf12db908a732428c80e7df2de0bef1b566e
RemoCharles <remo.gemperli@hotmail.ch>
2020-11-09 21:55:53 +0100

    removed stateful widget for about us site

--- lib/pages/about_us_site.dart
+++ lib/pages/about_us_site.dart
@@ -2,16 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:whes_tablet_app/localization/language_constants.dart';
 import 'package:whes_tablet_app/classes/styles.dart';
 
-class AboutUs extends StatefulWidget {
-  AboutUs({Key key}) : super(key: key);
-
-  @override
-  _AboutUsState createState() => _AboutUsState();
-}
-
-class _AboutUsState extends State<AboutUs> {
-  final GlobalKey<FormState> key = GlobalKey<FormState>();
-
+class AboutUs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
