2f999135246dd1859f1d37651de116b802ad37ce
RemoCharles <remo.gemperli@hotmail.ch>
2020-11-09 21:54:42 +0100

    created heritage.dart with all variables for popUp, Icon and HeritageDetail

---
+++ lib/classes/heritage.dart
@@ -0,0 +1,20 @@
+class Heritage {
+  final String titleDetail;
+  final String urlDetail;
+  final double bottomIcon;
+  final double rightIcon;
+  final String urlIcon;
+  final String titlePopUp;
+  final String textPopUp;
+  final String urlPopUp;
+
+  Heritage(
+      {this.titleDetail,
+      this.urlDetail,
+      this.bottomIcon,
+      this.rightIcon,
+      this.urlIcon,
+      this.titlePopUp,
+      this.textPopUp,
+      this.urlPopUp});
+}
