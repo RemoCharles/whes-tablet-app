6921bd810bfe230be65cd5b630aba18d6d69fd85
RemoCharles <remo.gemperli@hotmail.ch>
2020-11-13 10:00:49 +0100

    removed stateful

--- lib/pages/unesco_site.dart
+++ lib/pages/unesco_site.dart
@@ -2,16 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:whes_tablet_app/localization/language_constants.dart';
 import 'package:whes_tablet_app/classes/styles.dart';
 
-class Unesco extends StatefulWidget {
-  Unesco({Key key}) : super(key: key);
-
-  @override
-  _UnescoState createState() => _UnescoState();
-}
-
-class _UnescoState extends State<Unesco> {
-  final GlobalKey<FormState> key = GlobalKey<FormState>();
-
+class Unesco extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
